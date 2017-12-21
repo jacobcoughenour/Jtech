c26bb9e6805a052025ac9a329271718c4a21d57c
Jacob Coughenour <jacob15545@yahoo.com>
2017-12-21 04:38:11 -0500

    IUpdateable

---
+++ JTech/Core/IUpdateable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oxide.Plugins.JCore {
+
+	public interface IUpdateable {
+
+		void Update(int TickDelta);
+		
+	}
+
+}

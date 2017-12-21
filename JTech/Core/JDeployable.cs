dba4c7f8f42abee689fd6e4a916ef27837d2821d
Jacob Coughenour <jacob15545@yahoo.com>
2017-12-21 04:38:51 -0500

    JDeployable Placing and Spawning hooks

--- JTech/Core/JDeployable.cs
+++ JTech/Core/JDeployable.cs
@@ -14,6 +14,33 @@ namespace Oxide.Plugins.JCore {
 		// spawn/destroy 
 		// load/save
 
+		public int Id;
+		public ulong ownerId;
+		public string ownerName;
+		public bool isEnabled;
+		public float health;
+
+		private BaseEntity MainParent;
+		private List<BaseEntity> ChildEntities = new List<BaseEntity>();
+
+		public void SetMainParent(BaseEntity baseEntity) {
+			// TODO attach a component here
+
+			MainParent = baseEntity;
+			MainParent.enableSaving = false;
+		}
+
+		public void AddChildEntity(BaseEntity baseEntity) {
+			// TODO attach a component here
+
+			baseEntity.SetParent(baseEntity);
+			baseEntity.enableSaving = false;
+			ChildEntities.Add(baseEntity);
+		}
+		
+		
+		#region Placing
+
 		/// <summary>
 		/// Can User start placing this deployable?
 		/// Item requirements are already handled by UserInfo.
@@ -23,6 +50,55 @@ namespace Oxide.Plugins.JCore {
 		public static bool CanStartPlacing(UserInfo userInfo) {
 			return true;
 		}
+
+		/// <summary>
+		/// Called when player starts placing this deployable.
+		/// Use this for displaying instructions via userInfo.ShowMessage().
+		/// To stop placing you can either call userInfo.DonePlacing() or userInfo.CancelPlacing().
+		/// </summary>
+		public static void OnStartPlacing(UserInfo userInfo) {
+
+		}
+
+		/// <summary>
+		/// Called after userInfo.DonePlacing() or userInfo.CancelPlacing().
+		/// Use this to clean up anything left over from placing your deployable (visual aids, placeholder items, etc).
+		/// </summary>
+		public static void OnEndPlacing(UserInfo userInfo) {
+
+		}
+
+		/// <summary>
+		/// Called when player is placing this deployable and hits an entity with a hammer.
+		/// Use this if you want the player to select existing entities.
+		/// Use userInfo.placingdata to store selected entities or other data.
+		/// </summary>
+		public static void OnPlacingHammerHit(UserInfo userInfo, HitInfo hit) {
+
+		}
+		
+		/// <summary>
+		/// Spawn a deployable from placing command and set default values.
+		/// </summary>
+		/// <returns>If successfully placed</returns>
+		public virtual bool Place(UserInfo userInfo) {
+			// set deployable variables here
+			return Spawn();
+		}
+
+		#endregion
+		
+
+		public virtual bool Spawn() {
+			// spawn from variables
+			return false;
+		}
+
+
+		public virtual void Update(int TickDelta) {
+
+		}
+		
 
 	}
 

e5017b58605de2e765c795a79e47c5d61fc9805d
Jacob Coughenour <jacob15545@yahoo.com>
2018-01-01 22:45:30 -0500

    JInfo description parameter

--- JTech/Core/Attributes/JInfoAttribute.cs
+++ JTech/Core/Attributes/JInfoAttribute.cs
@@ -11,6 +11,7 @@ namespace Oxide.Plugins.JTechCore {
 		public InfoAttribute PluginInfo { get; }
 		public string Name { get; }
 		public string IconUrl { get; }
+		public string Description { get; }
 
 		/// <summary>
 		/// Info about this Custom JDeployable
@@ -18,10 +19,11 @@ namespace Oxide.Plugins.JTechCore {
 		/// <param name="pluginType">typeof(yourplugin)</param>
 		/// <param name="name">Name shown in menus and commands.</param>
 		/// <param name="iconUrl">Url for the icon shown in menus. Make it 200x200 with a transparent background.</param>
-		public JInfoAttribute(Type pluginType, string name, string iconUrl) {
+		public JInfoAttribute(Type pluginType, string name, string iconUrl, string description = null) {
 			this.PluginInfo = (InfoAttribute) GetCustomAttribute(pluginType, typeof(InfoAttribute));
 			this.Name = name;
 			this.IconUrl = iconUrl;
+			this.Description = description ?? string.Empty;
 		}
 	}
 }
